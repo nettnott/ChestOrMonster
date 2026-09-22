226eac5548ffdcb9cd2839f0fb49c0318daa1986
nettnott <nett.work.stud@gmail.com>
2026-09-22 09:09:22 +0300

    добавлен класс врага "!С юзер"

---
+++ ChestOrMonster/Model/Enemy/OneCUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChestOrMonster.Model.Enemy
+public class OneCUser : BaseEntity
+{
+    public override string Name { get; }
+    public override double Hp { get; protected set; }
+    public override double Atk { get; }
+    public override double Def { get; }
+    public override DamageType AttackType { get; }
+    public override StatusEffect Effect { get; protected set; }
+    protected virtual double CritRate { get; }
+
+    public OneCUser()
+    {
+        Name = "1С юзер";
+        Hp = 100;
+        Atk = 6;
+        Def = 7;
+        AttackType = DamageType.Usual;
+        Effect = StatusEffect.None;
+        CritRate = 0.3;
+    }
+
+    public override DamageInfo Attack()
+    {
+        double finalAtk = Atk;
+        if (_random.NextDouble() < CritRate)
+        {
+            finalAtk *= 1.1;
+        }
+        return new DamageInfo(finalAtk, AttackType);
+    }
